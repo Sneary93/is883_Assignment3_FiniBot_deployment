f0ba0bafdafd31bd0cad93f1cd910cea73a39013
Sneary93 <122059496+Sneary93@users.noreply.github.com>
2023-11-25 10:45:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ routes = [
 level= "Novice" #"Expert"
 
 from langchain.chat_models import ChatOpenAI
-llm = ChatOpenAI(api_key=openai_api_key, model = 'gpt-4')
+llm = ChatOpenAI(api_key=open_AI_key, model = 'gpt-4')
 
 destination_chains = {}
 for route in routes:
